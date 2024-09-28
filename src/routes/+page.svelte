15f2c2cc109620528215765e683054ffd6eab912
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-09-27 18:56:27 -0700

    added 7css via link instead, more work done on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const fidoughImgUrl =
+		'https://archives.bulbagarden.net/media/upload/thumb/8/82/0926Fidough.png/500px-0926Fidough.png';
+</script>
+
+<main class="has-scrollbar">
+	<h1>FIDOUGH FANSITE</h1>
+
+	<section id="table-of-contents">
+		<h2>Table of Contents</h2>
+		<ul class="tree-view has-container">
+			<li><a href="#about">About Fidough</a></li>
+			<li><a href="#abilities">Abilities</a></li>
+		</ul>
+	</section>
+
+	<section id="about">
+		<h2>About Fidough</h2>
+		<p>According to Bulbapedia:</p>
+
+		<blockquote>
+			"Fidough is a small canine Pokémon with a large head and stubby legs. The head contains small,
+			black beady eyes with rounded eyebrow-like growths on top; a snout with a rounded triangular
+			nose and a tiny mouth; and rounded ears that resemble pastries or bread buns. Fidough has a
+			large dewlap on its neck, on which it is able to rest its head while sleeping. Its back has
+			multiple protrusions that resemble a braid, and its tail is composed of two bulbous segments.
+			Its fur is off-white in coloration, with yellow accents on the top of its head, its eyebrows,
+			the insides of its ears, around its neck, and on its tail. Fidough's smooth, moist skin is
+			firm yet soft to the touch, and has elastic qualities; when agitated, Fidough will puff itself
+			up to appear larger, in an attempt to intimidate its opponents. Fidough's breath contains
+			yeast and is used to ferment things. Fidough has long been considered a companion for
+			humanity, because its yeast is useful for cooking."
+		</blockquote>
+		<img src={fidoughImgUrl} alt="Fidough" />
+	</section>
+
+	<section id="abilities">
+		<h2>Why Fidough is the Best</h2>
+		<p>Here's why Fidough is the best:</p>
+		<ul>
+			<li>It's cute</li>
+			<li>It's fluffy</li>
+			<li>IT'S FIDOUGH WHAT ELSE?</li>
+		</ul>
+	</section>
+</main>
+
+<footer>
+	<p>
+		©1995-{new Date().getFullYear()} Nintendo/Creatures Inc. /GAME FREAK inc. TM, ®, and character
+		names are trademarks of Nintendo.
+	</p>
+</footer>
